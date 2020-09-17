fbcfe7670ccca02ebd71e71132d8e2f8e27f1c73
Bobby Neal <rsneal01@gmail.com>
2020-09-17 17:19:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed =?",name, breed)
     if !dog.empty?
-      dog = song[0]
+      dog_data = dog[0]
       dog = Dog.new(song_data[0], song_data[1], song_data[2])
     else
       dog = self.create(name: name, album: album)
