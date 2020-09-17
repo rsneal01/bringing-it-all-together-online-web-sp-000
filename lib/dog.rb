5941c32fb7c69a7dcf2f86b3fb1f2ad1a2abf7d4
Bobby Neal <rsneal01@gmail.com>
2020-09-17 16:58:40 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,61 @@
+class Dog
+  
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  def initialize(id: nil, name: 'default', breed: 'default')
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE dogs
+        SQL
+        
+      DB[:conn].execute(sql)
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+ 
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+  
+  def self.create(name:, breed:)
+    dog = Dog.new(name, breed)
+    dog.save
+    dog
+  end
+  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+  def self.new_from_db(row)
+    new_dog = self.new(row[0], row[1], row[2]) 
+  end
+  
+end
